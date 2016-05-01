fa6a0d433e3085107d40ef8c8906c52751cd34b2
Trevor Pham <trevor.dt.pham@gmail.com>
2016-05-01 11:34:52 -0700

    can now call watson tone analyzer

--- controllers/watson.js
+++ controllers/watson.js
@@ -1,5 +1,31 @@
-var watson = require('watson-developer-cloud')
+require('dotenv').load();
+var watson = require('watson-developer-cloud');
+
+function textAnalyze(req, res, next) {
+  tone_analyzer.tone({ text: 'Greetings from Watson Developer Cloud!'}, function(err, tone) {
+    if (err) console.log(err);
+    else {
+      res.send(JSON.stringify(tone, null, 2))
+    }
+  })
+}
 
 var tone_analyzer = watson.tone_analyzer({
-  user
-})
+  username: process.env.USERNAME_WATSON,
+  password: process.env.PASSWORD_WATSON,
+  version:  'v3-beta',
+  version_date: '2016-02-11'
+});
+
+module.exports = {
+  show: show
+}
+
+function show(req, res, next) {
+  tone_analyzer.tone({ text: 'Greetings from Watson Developer Cloud!'}, function(err, tone) {
+    if (err) console.log(err);
+    else {
+      res.json(tone)
+    }
+  })
+}
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var watsonController = require('../controllers/watson')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
+router.get('/watson', watsonController.show)
 
 module.exports = router;
