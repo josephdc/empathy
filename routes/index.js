f581eb95cedbb1168c08900c0f6c0492ebb7c5eb
Trevor Pham <trevor.dt.pham@gmail.com>
2016-05-02 09:58:26 -0700

    add more packages

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 var watsonController = require('../controllers/watson');
-var tweetsController = require('../controllers/tweets')
+var tweetsController = require('../controllers/tweets');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// test pages
 router.get('/watson', watsonController.index)
 router.post('/watson', watsonController.analyze)
 // router.get('/tweets', tweetsController.index)
