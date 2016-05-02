ca3f8f95223f936e6307ad24206cf41d81b54e9c
Carlos Allende <carlosallendesanz@gmail.com>
2016-05-02 16:25:38 -0700

    add user to database mongo

--- models/user.js
+++ models/user.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
 var Report = require('./report');
+var schema = mongoose.Schema
 
 var userSchema = new mongoose.Schema({
   id: Number,
   username: String,
-  twitter_handle: String,
-  reports: [{type: Schema.Types.ObjectId, ref: "Report"}]
+  displayName: String,
+  reports: [{type: schema.Types.ObjectId, ref: "Report"}]
 })
 
 var User = mongoose.model("User", userSchema);
